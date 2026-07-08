224cd9ec12d67ab9a51a0cfc0b8f669637951c6c
blaiztm <timurbryz@gmail.com>
2026-07-08 13:28:01 +0300

    Added start command

--- MLPal/main.py
+++ MLPal/main.py
@@ -1,10 +1,16 @@
 import asyncio
 from aiogram import Bot, Dispatcher
+from aiogram.filters import Command
 
-TOKEN = "8715118884:AAHlLD-8v42xFxr4kXgCAOqWJdL4cCBKZEE" 
+TOKEN = "8715118884:AAFKD-Then8321t_DZqiF8JxfsalucGn9Ws" 
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
+
+
+@dp.message(Command("start"))
+async def cmd_start(message):
+    await message.answer("Hi fellow ML learner, what is your name?")
 
 async def main():
     await dp.start_polling(bot)
